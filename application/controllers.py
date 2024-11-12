4ecffa23fde6f1733a9b273fc6f52ca267463dc6
utsavudit <mishrautsavudit@gmail.com>
2024-11-12 14:47:40 +0530

    utsav branch

--- application/controllers.py
+++ application/controllers.py
@@ -28,7 +28,7 @@ def deleteEvent(event_id):
 
 #check allowed image extensions
 def allowed_file(filename):
-    ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+    ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif','webp'}
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 #methods imported from flask module
